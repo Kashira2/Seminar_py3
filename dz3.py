0c4e7e1ada417d65d9fa9b1bf2782bd18189c096
Kashira2 <nekit.egorov@icloud.com>
2022-12-23 21:56:30 +0300

    dz3

--- dz3.py
+++ dz3.py
@@ -1,3 +1,26 @@
 # Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов, отличной от 0.
 # Пример:
 # [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+
+import random
+
+size = int(input('Задайте размер списка: '))
+my_list = []
+
+for i in range(size):
+    my_list.append(round(random.uniform(0,10), 4))
+print(my_list)
+max = int(0)
+min = int(5000)
+for i in range(size):
+    my_list[i] = str(my_list[i])
+    number = my_list[i].split('.')
+    number_after_dot = int(number[1])
+    if max < number_after_dot:
+        max = number_after_dot
+    elif min > number_after_dot:
+        min = number_after_dot
+
+print(f'Максимальное значение дробной части элементов равно {max}')
+print(f'Минимальное значение дробной части элементов равно {min}')
+print(f'Разница между максимальным и минимальным значением дробной части элементов равно {max - min}') 
